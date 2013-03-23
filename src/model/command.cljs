f46086a4f6e6b6fcd1acdb02f6a3a4fa874d6695
Benjamin Teuber <bsteuber@gmail.com>
2013-03-23 17:06:13 +0100

    command compiler for serializable patches

---
+++ src/model/command.cljs
@@ -0,0 +1,31 @@
+(ns model.command)
+
+(defmulti command->fn
+  (fn [key & _]
+    key))
+
+(defn cmd->fn [cmd]
+  (apply command->fn cmd))
+
+(defn apply-cmd [cmd data]
+  (apply (cmd->fn cmd) data))
+
+(defmethod command->fn :set [_ new-value]
+  (constantly new-value))
+
+(defmethod command->fn :inc [_]
+  inc)
+
+(defmethod command->fn :dec [_]
+  inc)
+
+(defmethod command->fn :update-in [_ path cmd]
+  #(update-in % path (cmd->fn cmd)))
+
+(defn test []
+  (assert
+   (= (apply-cmd [:inc] 5)
+      6))
+  (assert
+   (= (apply-cmd [:update-in [:foo] [:set 42]] {:foo 4})
+      {:foo 42})))
